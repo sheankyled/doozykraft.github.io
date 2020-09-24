a6e385c9d1ca0b5138657e71165defe1ba4e8ca4
sheankyled <sheankyle.daraman@gmail.com>
2020-09-24 14:15:33 +0800

    New files

---
+++ script.js
@@ -0,0 +1,9 @@
+var userName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var Age = prompt("How old are you??");
+
+var fullName = userName + " " + lastName
+
+console.log("Your full name is " + fullName);
+// console.log("Your full name is " + userName + " " + lastName);
+console.log("You are " + Age + " Years old");
